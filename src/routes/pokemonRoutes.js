fd33ca2a0c26042f74ce61c5d5fa770bc4a951a3
MJ-088 <jagtapmanasi24@gmail.com>
2025-11-26 17:14:55 +0530

    Delete src directory

--- src/routes/pokemonRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { fetchPokemon } from "../controller/pokemonController.js"; // matches export name
-
-const router = express.Router();
-
-router.get("/pokemon/:name", fetchPokemon);
-
-export default router;
--- src/services/pokemonApi.js
+++
@@ -1,7 +0,0 @@
-const API_URL = "http://localhost:4000/api/pokemon"; // match your backend port
-
-export async function fetchPokemon(name) {
-  const res = await fetch(`${API_URL}/${name.toLowerCase()}`);
-  if (!res.ok) throw new Error("Pokemon not found");
-  return res.json();
-}
